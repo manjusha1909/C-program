f15776dae52acdb14ddd645dacf94cda71ae4eae
manjushaukande09 <118713767+manjushaukande09@users.noreply.github.com>
2022-11-21 12:43:26 +0530

    Update fact.c

--- fact.c
+++ fact.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
+void fact()
 main()
 {
-  int i,n,fact;
+  int i,n,fact=1;
   printf("\n enter the number");
   scanf("%d",&n);
   for(i=0;i<=n;i++)
@@ -9,3 +10,11 @@ main()
     fact=fact*i;
   }
   printf("\n it is fact",fact);
+  
+  void add()
+  {
+    int a,b,c;
+    printf("\n enter  the two no");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("\n addioton c=",c);
